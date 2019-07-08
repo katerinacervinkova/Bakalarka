5797dcf5e17a5386939c0a7980b0bf6f0b08011e
katerinacervinkova <37743398+katerinacervinkova@users.noreply.github.com>
2019-07-08 08:58:42 +0200

    menuplayerlist and playerrow comments

--- Age/Assets/Menu/MenuPlayerList.cs
+++ Age/Assets/Menu/MenuPlayerList.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class that handles color distribution between players.
+/// </summary>
 public class MenuPlayerList : MonoBehaviour {
 
     private Dictionary <Color, bool> ColorsAvailable = new Dictionary<Color, bool>();
     private Color[] Colors = new Color[] { Color.red, Color.yellow, Color.green, Color.blue, Color.cyan, Color.magenta, Color.black};
 
+    /// <summary>
+    /// Gets some available color for new player.
+    /// </summary>
+    /// <param name="index">color index used for getting the next color</param>
+    /// <returns>color for the player to use</returns>
     public Color GetColor(out int index)
     {
         index = 0;
@@ -21,6 +28,11 @@ public class MenuPlayerList : MonoBehaviour {
         return Color.black;
     }
 	
+    /// <summary>
+    /// Gets next available color for player.
+    /// </summary>
+    /// <param name="index">color index</param>
+    /// <returns>color for the player to use</returns>
     public Color GetNextColor(ref int index)
     {
         ColorsAvailable[Colors[index]] = true;
@@ -37,6 +49,10 @@ public class MenuPlayerList : MonoBehaviour {
         return Colors[index];
     }
 
+    /// <summary>
+    /// Sets the color available again.
+    /// </summary>
+    /// <param name="color">color to be made available</param>
     public void RemoveColor(Color color)
     {
         ColorsAvailable[color] = true;
--- Age/Assets/Menu/PlayerRow.cs
+++ Age/Assets/Menu/PlayerRow.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Class that deals with UI showing the state of the player initialization.
+/// </summary>
 public class PlayerRow : MonoBehaviour {
 
     public MenuPlayer player;
@@ -12,36 +15,60 @@ public class PlayerRow : MonoBehaviour {
     [SerializeField]
     private Button removeButton;
 
+    /// <summary>
+    /// Initializes the player name.
+    /// </summary>
     private void Start()
     {
         player.ChangeName(playerName.text);
     }
 
+    /// <summary>
+    /// Changes the color in the UI.
+    /// </summary>
+    /// <param name="color">new player color</param>
     public void SetColor(Color color)
     {
         colorImage.color = color;
     }
 
+    /// <summary>
+    /// Changes the player name in the UI.
+    /// </summary>
+    /// <param name="name">new player name</param>
     public void SetName(string name)
     {
         playerName.text = name;
     }
 
+    /// <summary>
+    /// Deals with player clicking the color button.
+    /// </summary>
     public void OnColorClick()
     {
         player.ChangeColor();
     }
 
+    /// <summary>
+    /// Deals with player changing the player name.
+    /// </summary>
+    /// <param name="Name">new player name</param>
     public void OnNameChange(string Name)
     {
         player.ChangeName(Name);
     }
 
+    /// <summary>
+    /// Deals with player clicking the remove button.
+    /// </summary>
     public void OnRemoveButtonClick()
     {
         player.RemovePlayer();
     }
 
+    /// <summary>
+    /// Sets the interactivity for player that has authority.
+    /// </summary>
     public void SetInteractivity()
     {
         playerName.readOnly = false;
