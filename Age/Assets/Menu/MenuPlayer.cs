47e4e751c869cb606d10a96c80dfa0fcbafef18a
katerinacervinkova <37743398+katerinacervinkova@users.noreply.github.com>
2019-07-08 08:49:15 +0200

    menuplayer comments

--- Age/Assets/Menu/MenuPlayer.cs
+++ Age/Assets/Menu/MenuPlayer.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Used for player representation in the menu
+/// </summary>
 public class MenuPlayer : NetworkBehaviour {
 
     [SyncVar(hook = "OnNameChange")]
@@ -22,6 +24,9 @@ public class MenuPlayer : NetworkBehaviour {
     [SerializeField]
     private Player playerPrefab;
 
+    /// <summary>
+    /// Inits the references.
+    /// </summary>
     public override void OnStartClient()
     {
         base.OnStartClient();
@@ -33,6 +38,9 @@ public class MenuPlayer : NetworkBehaviour {
         playerRow.player = this;
     }
 
+    /// <summary>
+    /// Inits the interactivity and sets color.
+    /// </summary>
     public override void OnStartAuthority()
     {
         playerRow.SetInteractivity();
@@ -54,41 +62,65 @@ public class MenuPlayer : NetworkBehaviour {
         }
     }
 
+    /// <summary>
+    /// Removes thís player.
+    /// </summary>
     public void RemovePlayer()
     {
         menuManager.RemovePlayer(playerControllerId);
     }
 
+    /// <summary>
+    /// Changes the color to the next available one.
+    /// </summary>
     public void ChangeColor()
     {
         if (hasAuthority)
             CmdChangeColor();
     }
 
+    /// <summary>
+    /// Passes the name change to the server.
+    /// </summary>
+    /// <param name="Name">new player name</param>
     public void ChangeName(string Name)
     {
         if (hasAuthority)
             CmdChangeName(Name);
     }
 
+    /// <summary>
+    /// Reacts to the color change by updating the UI.
+    /// </summary>
+    /// <param name="newColor">new player color</param>
     private void OnColorChange(Color newColor)
     {
         color = newColor;
         playerRow?.SetColor(color);
     }
 
+    /// <summary>
+    /// Reacts to the name change by updating the UI.
+    /// </summary>
+    /// <param name="newName">new player name</param>
     private void OnNameChange(string newName)
     {
         Name = newName;
         playerRow?.SetName(Name);
     }
 
+    /// <summary>
+    /// Sets the next available color on the server.
+    /// </summary>
     [Command]
     private void CmdSetColor()
     {
         color = playerList.GetColor(out colorIndex);
     }
 
+    /// <summary>
+    /// Replaces this player by actual game player.
+    /// </summary>
     [Command]
     private void CmdStartGameScene()
     {
@@ -100,18 +132,28 @@ public class MenuPlayer : NetworkBehaviour {
         NetworkServer.Destroy(gameObject);
     }
 
+    /// <summary>
+    /// Changes the player color to the next available one on the server.
+    /// </summary>
     [Command]
     private void CmdChangeColor()
     {
         color = playerList.GetNextColor(ref colorIndex);
     }
 
+    /// <summary>
+    /// Changes the player name on the server.
+    /// </summary>
+    /// <param name="Name">new player name</param>
     [Command]
     private void CmdChangeName(string Name)
     {
         this.Name = Name;
     }
 
+    /// <summary>
+    /// Destroys depending objects and listening for events.
+    /// </summary>
     private void OnDestroy()
     {
         SceneManager.activeSceneChanged -= SceneChanged;
