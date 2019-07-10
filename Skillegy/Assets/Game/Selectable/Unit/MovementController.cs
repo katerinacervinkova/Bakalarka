eb85e03ce04b44c457c01cccb538f045afdf99a1
katerinacervinkova <37743398+katerinacervinkova@users.noreply.github.com>
2019-07-10 22:43:53 +0200

    movement controller comments

--- Skillegy/Assets/Game/Selectable/Unit/MovementController.cs
+++ Skillegy/Assets/Game/Selectable/Unit/MovementController.cs
@@ -10,19 +10,12 @@ public class MovementController : NetworkBehaviour {
     private AIUnetPath aiUnetPath;
     private Unit unit;
 
-    // variables related to movement
     public Vector3 destination = Vector3.positiveInfinity;
+    public bool IsMoving => !float.IsPositiveInfinity(destination.x);
+
 
     private bool causedShowingTarget = false;
-    public bool IsMoving => !float.IsPositiveInfinity(destination.x);
     private Vector3 lastPosition;
-
-    public void OnTargetReached()
-    {
-        aiUnetPath.destination = Vector3.positiveInfinity;
-        unit.OnTargetReached();
-        destination = Vector3.positiveInfinity;
-    }
 
     private void Awake()
     {
@@ -34,43 +27,50 @@ public class MovementController : NetworkBehaviour {
     {
         if (hasAuthority && aiUnetPath.ShouldRecalculatePath)
         {
+            // changes the destination a little bit if the unit cannot move in its direction
             if (!HasMoved() && Vector3.Distance(unit.transform.position, destination) > aiUnetPath.endReachedDistance * 2)
             {
                 var v = Random.insideUnitCircle * aiUnetPath.endReachedDistance;
                 aiUnetPath.destination = destination + new Vector3(v.x, 0, v.y);
                 aiUnetPath.endReachedDistance += 0.1f;
             }
+            // unit is not blocked anymore, returns variable to its original state
             else if (HasMoved())
                 aiUnetPath.endReachedDistance = 0.6f;
+
+            // starts path calculating on the server
             CmdSearchPath(aiUnetPath.destination);
+
             lastPosition = transform.position;
         }
     }
 
+    public void Go(Vector3 destination)
+    {
+        // sets new destination
+        this.destination = destination;
+        ShowTarget();
+        aiUnetPath.destination = destination;
+        aiUnetPath.endReachedDistance = 0.6f;
+    }
+
     private bool HasMoved()
     {
+        // returns true if the unit has moved since last frame
         return Vector3.Distance(lastPosition, transform.position) > 0.1;
     }
 
     [Command]
     private void CmdSearchPath(Vector3 destination)
     {
+        // search path on server
         aiUnetPath.destination = destination;
         aiUnetPath.SearchPath();
     }
 
-    [ClientRpc]
-    private void RpcOnPathComplete()
-    {
-        List<Vector3> vPath = new List<Vector3>();
-        foreach (var vector in vectorPath)
-            vPath.Add(vector);
-        var path = ABPath.FakePath(vPath);
-        aiUnetPath.RpcOnPathComplete(path);
-    }
-
     public void OnPathComplete(Path p)
     {
+        // synchronizes the path
         if (isServer && !p.error)
         {
             vectorPath.Clear();
@@ -80,13 +80,25 @@ public class MovementController : NetworkBehaviour {
         }
     }
 
-    public void Go(Vector3 destination)
+    [ClientRpc]
+    private void RpcOnPathComplete()
     {
-        this.destination = destination;
-        ShowTarget();
-        aiUnetPath.destination = destination;
-        aiUnetPath.endReachedDistance = 0.6f;
+        // reconstructs the path and calls the original OnPathComplete
+        List<Vector3> vPath = new List<Vector3>();
+        foreach (var vector in vectorPath)
+            vPath.Add(vector);
+        var path = ABPath.FakePath(vPath);
+        aiUnetPath.RpcOnPathComplete(path);
     }
+
+    public void OnTargetReached()
+    {
+        aiUnetPath.destination = Vector3.positiveInfinity; 
+        //informs unit that the job is completed
+        unit.OnTargetReached();
+        destination = Vector3.positiveInfinity;
+    }
+
 
     /// <summary>
     /// Called when the unit gets deselected or arrives at its destination. Hides the target.
