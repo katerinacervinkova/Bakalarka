f6d633059c0e26dff98c0cb856e4ce61cd3e53f0
katerinacervinkova <37743398+katerinacervinkova@users.noreply.github.com>
2019-07-15 10:03:20 +0200

    Purchases speed

--- Skillegy/Assets/Game/Selectable/Purchases/PlayerPurchases.cs
+++ Skillegy/Assets/Game/Selectable/Purchases/PlayerPurchases.cs
@@ -122,72 +122,72 @@ public class PlayerPurchases : MonoBehaviour {
 
             #region main building purchases
             [PurchasesEnum.Unit] = new LoadingPurchase(
-                1, "Unit", player.playerControllerId, unitImage, "Create a unit",
+                0.5f, "Unit", player.playerControllerId, unitImage, "Create a unit",
                 s => player.CreateUnit(s as Building),
                 s => true, food: 150, wood: 50, gold: 20, population: 1),
 
             [PurchasesEnum.StoneAge] = new LoadingPurchase(
-                1, "Stone Age", player.playerControllerId, stoneAgeImage, "Advance to the Stone Age. Grants access to new advancements and foundations.",
+                0.1f, "Stone Age", player.playerControllerId, stoneAgeImage, "Advance to the Stone Age. Grants access to new advancements and foundations.",
                 s => PlayerState.Age = PlayerState.AgeEnum.Stone,
                 s => true, food: 2000, wood: 0, gold: 2000, oneTimePurchase: true),
 
             [PurchasesEnum.IronAge] = new LoadingPurchase(
-                1, "Iron Age", player.playerControllerId, ironAgeImage, "Advance to the Iron Age. Grants access to new advancements and foundations.",
+                0.08f, "Iron Age", player.playerControllerId, ironAgeImage, "Advance to the Iron Age. Grants access to new advancements and foundations.",
                 s => PlayerState.Age = PlayerState.AgeEnum.Iron,
                 s => ReachedAge(PlayerState.AgeEnum.Stone), food: 5000, wood: 1000, gold: 3000, oneTimePurchase: true),
 
             [PurchasesEnum.DiamondAge] = new LoadingPurchase(
-                1, "Diamond Age", player.playerControllerId, diamondAgeImage, "Advance to the Diamond Age. Grants access to new advancements and foundations.",
+                0.06f, "Diamond Age", player.playerControllerId, diamondAgeImage, "Advance to the Diamond Age. Grants access to new advancements and foundations.",
                 s => PlayerState.Age = PlayerState.AgeEnum.Diamond,
                 s => ReachedAge(PlayerState.AgeEnum.Iron), food: 10000, wood: 2000, gold: 7000, oneTimePurchase: true),
             #endregion
 
             #region library purchases
             [PurchasesEnum.Books1] = new LoadingPurchase(
-                1, "Books 1", player.playerControllerId, books1Image, "Supply library with new books. Increases maximum intelligence.",
+                0.3f, "Books 1", player.playerControllerId, books1Image, "Supply library with new books. Increases maximum intelligence.",
                 s => { (s as Library).maxIntelligence = 20; (s as Library).Books1 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Stone),
                 food: 0, wood: 500, gold: 500, oneTimePurchase: true),
 
             [PurchasesEnum.Books2] = new LoadingPurchase(
-                1, "Books 2", player.playerControllerId, books2Image, "Supply library with new books. Increases maximum intelligence.",
+                0.3f, "Books 2", player.playerControllerId, books2Image, "Supply library with new books. Increases maximum intelligence.",
                 s => { (s as Library).maxIntelligence = 35; (s as Library).Books2 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Iron) && (s as Library).Books1,
                 food: 500, wood: 2000, gold: 2500, oneTimePurchase: true),
 
             [PurchasesEnum.Books3] = new LoadingPurchase(
-                1, "Books 3", player.playerControllerId, books3Image, "Supply library with new books. Increases maximum intelligence.",
+                0.25f, "Books 3", player.playerControllerId, books3Image, "Supply library with new books. Increases maximum intelligence.",
                 s => { (s as Library).maxIntelligence = 55; (s as Library).Books3 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Iron) && PlayerState.Get(player.playerControllerId).units.Count > 30 && (s as Library).Books2,
                 food: 1000, wood: 3000, gold: 3500, oneTimePurchase: true),
 
             [PurchasesEnum.Books4] = new LoadingPurchase(
-                1, "Books 4", player.playerControllerId, books4Image, "Supply library with new books. Increases maximum intelligence.",
+                0.25f, "Books 4", player.playerControllerId, books4Image, "Supply library with new books. Increases maximum intelligence.",
                 s => { (s as Library).maxIntelligence = 75; (s as Library).Books4 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Diamond) && PlayerState.buildings.Where(b => b is Mill).Any() && (s as Library).Books3,
                 food: 2000, wood: 5000, gold: 5000, oneTimePurchase: true),
 
             [PurchasesEnum.Books5] = new LoadingPurchase(
-                1, "Books 5", player.playerControllerId, books5Image, "Supply library with new books. Increases maximum intelligence.",
+                0.2f, "Books 5", player.playerControllerId, books5Image, "Supply library with new books. Increases maximum intelligence.",
                 s => { (s as Library).maxIntelligence = 100; (s as Library).Books5 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Diamond) && (s as Library).Books4 && PlayerState.MedicineBooks2
                 && PlayerState.buildings.Where(b => b is Barracks && (b as Barracks).Gear5).Any(),
                 food: 3000, wood: 6000, gold: 7000, oneTimePurchase: true),
 
             [PurchasesEnum.BuildingBooks] = new LoadingPurchase(
-                1, "Building Books", player.playerControllerId, buildingBooksImage, "Supply library with new books about building. Enables building skill development in library.",
+                0.3f, "Building Books", player.playerControllerId, buildingBooksImage, "Supply library with new books about building. Enables building skill development in library.",
                 s => PlayerState.BuildingBooks = true,
                 s => ReachedAge(PlayerState.AgeEnum.Stone) && (s as Library).Books2 && PlayerState.buildings.Where(b => b is Sawmill).Any(),
                 food: 500, wood: 1500, gold: 1500, oneTimePurchase: true),
 
             [PurchasesEnum.MedicineBooks1] = new LoadingPurchase(
-                1, "Medicine Books 1", player.playerControllerId, medicineBooks1Image, "Supply library with new books about medicine. Enables healing skill development in library.",
+                0.3f, "Medicine Books 1", player.playerControllerId, medicineBooks1Image, "Supply library with new books about medicine. Enables healing skill development in library.",
                 s => PlayerState.MedicineBooks1 = true,
                 s => ReachedAge(PlayerState.AgeEnum.Iron) && (s as Library).Books3 && PlayerState.buildings.Where(b => b is Infirmary).Any(),
                 food: 1500, wood: 500, gold: 1500, oneTimePurchase: true),
 
             [PurchasesEnum.MedicineBooks2] = new LoadingPurchase(
-                1, "Medicine Books 2", player.playerControllerId, medicineBooks2Image, "Supply library with new books about medicine. Increases doctors' effectivity.",
+                0.25f, "Medicine Books 2", player.playerControllerId, medicineBooks2Image, "Supply library with new books about medicine. Increases doctors' effectivity.",
                 s => PlayerState.MedicineBooks2 = true,
                 s => ReachedAge(PlayerState.AgeEnum.Iron) && PlayerState.MedicineBooks1,
                 food: 2500, wood: 500, gold: 2500, oneTimePurchase: true),
@@ -214,31 +214,31 @@ public class PlayerPurchases : MonoBehaviour {
 
             #region barracks purchases
             [PurchasesEnum.Gear1] = new LoadingPurchase(
-                1, "Gear 1", player.playerControllerId, gear1Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
+                0.3f, "Gear 1", player.playerControllerId, gear1Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
                 s => { (s as Barracks).maxSwordsmanship = 20; (s as Barracks).Gear1 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Stone),
                 food: 200, wood: 500, gold: 500, oneTimePurchase: true),
 
             [PurchasesEnum.Gear2] = new LoadingPurchase(
-                1, "Gear 2", player.playerControllerId, gear2Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
+                0.3f, "Gear 2", player.playerControllerId, gear2Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
                 s => { (s as Barracks).maxSwordsmanship = 35; (s as Barracks).Gear2 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Stone) && (s as Barracks).Gear1 && PlayerState.buildings.Where(b => b is Library && (b as Library).Books1).Any(),
                 food: 300, wood: 700, gold: 700, oneTimePurchase: true),
 
             [PurchasesEnum.Gear3] = new LoadingPurchase(
-                1, "Gear 3", player.playerControllerId, gear3Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
+                0.25f, "Gear 3", player.playerControllerId, gear3Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
                 s => { (s as Barracks).maxSwordsmanship = 55; (s as Barracks).Gear3 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Iron) && (s as Barracks).Gear2,
                 food: 700, wood: 1000, gold: 1500, oneTimePurchase: true),
 
             [PurchasesEnum.Gear4] = new LoadingPurchase(
-                1, "Gear 4", player.playerControllerId, gear4Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
+                0.25f, "Gear 4", player.playerControllerId, gear4Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
                 s => { (s as Barracks).maxSwordsmanship = 75; (s as Barracks).Gear4 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Iron) && (s as Barracks).Gear3 && PlayerState.buildings.Where(b => b is Library && (b as Library).Books2).Any(),
                 food: 1000, wood: 1500, gold: 2500, oneTimePurchase: true),
 
             [PurchasesEnum.Gear5] = new LoadingPurchase(
-                1, "Gear 5", player.playerControllerId, gear5Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
+                0.2f, "Gear 5", player.playerControllerId, gear5Image, "Supply barracks with better gear. Increases maximum swordsmanship.",
                 s => { (s as Barracks).maxSwordsmanship = 100; (s as Barracks).Gear5 = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Diamond) && (s as Barracks).Gear4 && PlayerState.buildings.Where(b => b is Library && (b as Library).Books4).Any(),
                 food: 2000, wood: 3000, gold: 6000, oneTimePurchase: true),
@@ -246,13 +246,13 @@ public class PlayerPurchases : MonoBehaviour {
 
             #region mill
             [PurchasesEnum.Flour] = new LoadingPurchase(
-                1, "Flour", player.playerControllerId, flourImage, "Supply mill with better gear. Increases gathering speed.",
+                0.3f, "Flour", player.playerControllerId, flourImage, "Supply mill with better gear. Increases gathering speed.",
                 s => { (s as Mill).Speed *= 2; (s as Mill).Flour = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Stone),
                 food: 500, wood: 1000, gold: 1500, oneTimePurchase: true),
 
             [PurchasesEnum.Bread] = new LoadingPurchase(
-                1, "Bread", player.playerControllerId, breadImage, "Supply mill with better gear. Increases gathering speed.",
+                0.3f, "Bread", player.playerControllerId, breadImage, "Supply mill with better gear. Increases gathering speed.",
                 s => { (s as Mill).Speed *= 2; (s as Mill).Bread = true; },
                 s => ReachedAge(PlayerState.AgeEnum.Iron) && (s as Mill).Flour,
                 food: 1000, wood: 1500, gold: 2000, oneTimePurchase: true),
